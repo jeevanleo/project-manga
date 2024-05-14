e8c8493831949f9751d1a11ace0c6d30f599144d
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-05-14 20:43:25 +0530

    Update book.route.js

--- Backend/route/book.route.js
+++ Backend/route/book.route.js
@@ -3,6 +3,6 @@ import { getBook } from "../controller/book.controller.js";
 
 const router = express.Router();
 
-router.get("https://project-manga.vercel.app/", getBook);
+router.get("https://project-manga-frontend.vercel.app/", getBook);
 
 export default router;
