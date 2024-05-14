b777d9d302c94b26e566acd0074c78541b9e2a3a
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-05-14 22:45:23 +0530

    Update user.route.js

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signup, login } from "../controller/user.controller.js";
 const router = express.Router();
 
-router.post("https://project-manga-frontend.vercel.app/signup", signup);
-router.post("https://project-manga-frontend.vercel.app/login", login);
+router.post("https://project-manga.vercel.app/signup", signup);
+router.post("https://project-manga.vercel.app/login", login);
 
 export default router;
