73abf36b84227a0488d1c3a2b06e7b8ba10ead93
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-05-17 22:12:41 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://project-manga-frontend.vercel.app//book");
+        const res = await axios.get("https://project-manga-frontend.vercel.app/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
